72a6695161680a82932ae0dfa14fc0e616a07ff9
Matheus <matheuswortiz@gmail.com>
2025-02-11 14:30:15 -0400

    tentando passar2

--- Login.php
+++ Login.php
@@ -4,149 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de Login</title>
-    <link rel="stylesheet" href="./Trabalho/css/página/Login.css">
+    <link rel="stylesheet" href="trabalho/css/página/Login.css">
 
 </head>
 <body>
-<style>
-    .parte-cima{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-input{
-    font-size:15px;
-    
-}
-footer{
-    color:white;
-}
-nav {
-    margin: 10px 0;
-    
-}
-.link{ color: #fff; 
-     padding: 15px;
-     margin: 0 15px;
-      text-decoration: none;
-      font-weight: bold; 
-      border-radius: 5px; 
-      transition: background-color 0.3s, color 0.3s;
-     } 
-     .link:hover { 
-        background-color: #007bff; 
-        color: #fff; 
-    } 
+<?php
+include "header.php";
 
-.pesquisa {
-    height:30px;
-    width: 600px;
-    padding: 10px;
-    border: 2px solid #ddd;
-    border-radius: 25px 0 0 25px;
-    outline: none;
-    font-size: 16px;
-    transition: border-color 0.3s;
-}
-.logotipo{
-    width: 100px;
-    height: 100px;
-}
-body {
-            display: flex;
-            flex-direction: column;
-            min-height: 120vh;
-            margin: 0;
-            font-family: Arial, sans-serif;
-            background-image:url('fundo.png');
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position:  center;
-        
-        }
-        header{
-            height:100px;
-        }
-header, footer {
-            background-color: rgb(78, 78, 78);
-            padding: 1rem;
-            text-align: center;
-        }
- main {
-            flex: 1;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 1rem;
-        }
-.login-container {
-    color: white;
-    background-color: black;
-    position: absolute;
-    top: 60%;
-    left: 50%;
-    transform: translate(-50%, -50%) ;
-    padding: 60px;
-    border-radius: 10px;
-    outline: none;
-        }
-.login-container h2 {
-            margin-bottom: 1rem;
-        }
-.login-container label {
-            display: block;
-            margin-bottom: 1rem;
-        }
-.login-container input {
-            width: 250px;
-            padding: 0.5rem;
-            margin-bottom: 1rem;
-            border: 1px solid #ccc;
-            border-radius: 3px;
-        }
-.login-container button {
-            width: 270px;
-            padding: 1rem;
-            background-color: #007bff;
-            border: none;
-            color: #fff;
-            border-radius: 10px;
-            cursor: pointer;
-        }
-.login-container button:hover {
-            background-color: #0056b3;
-        }
-
-        .parte-cima h2{
-    color:white;
-    display: inline;
-    font-size:40px;
-
-}
-.h1{
-    color:black
-}
-.barra-logo{
-    text-decoration:none;
-}
-</style>
-
-
-<header>
-    <nav>
-
-    <div class="parte-cima">
-            <div>
-                <a class="barra-logo" href="Front.php">
-                    <h2 class="h1">Fashion</h2><h2>Online</h2>
-                </a>
-            </div>
-       <div>
-
-    <a class="link" href="Front.php">Home</a>
-    <a class="link" href="carrinho.php">Carrinho</a>
-    <a class="link" href="Login.php">Login</a>
-</nav>
+?>
     </header>
     <main>
 
@@ -164,8 +29,9 @@ header, footer {
             <a class="cadastro" href="#" class="link">Não tem uma conta? Cadastre-se aqui</a>
         </div>
     </main>
-    <footer>
-        <p>&copy; 2024 Loja Exemplo. Todos os direitos reservados.</p>
-    </footer>
+    <?php
+include "footer.php";
+
+?>
 </body>
 </html>
